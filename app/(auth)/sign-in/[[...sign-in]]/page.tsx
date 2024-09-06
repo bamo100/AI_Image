aee7bad30c6a96569be8077068249ac1bb324c0c
bamo100 <bankolediamond10@gmail.com>
2024-09-06 17:46:34 +0100

    feat: first commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignIn, SignInButton, SignUp, SignedIn } from "@clerk/nextjs";
+import React from 'react'
+
+const SignInPage = () => {
+  return <SignIn />
+}
+
+export default SignInPage
---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignUp, UserButton } from "@clerk/nextjs";
+import React from 'react'
+
+const SignUpPage = () => {
+    return <SignUp />
+}
+
+export default SignUpPage
