6c7f12668be55719ad7d63d8472ab47a04d3ffb9
bamo100 <bankolediamond10@gmail.com>
2024-09-09 18:26:20 +0100

    fix: fix middleware

--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,5 @@ export default clerkMiddleware((auth, request) => {
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
