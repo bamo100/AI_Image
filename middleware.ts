b3a6e6a68f4558ff2f14684205b140a578544385
bamo100 <bankolediamond10@gmail.com>
2024-09-10 14:08:19 +0100

    fix: quick fix

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,26 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([ '/', '/sign-in(.*)', '/sign-up(.*)', 'api/webhooks/clerk'])
+// const isPublicRoute = createRouteMatcher([ '/', '/sign-in(.*)', '/sign-up(.*)', 'api/webhooks/clerk'])
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect()
+// export default clerkMiddleware((auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     auth().protect()
+//   }
+// })
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/client"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
   }
-})
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
