8f6fd7c228e08ee25408ecc233679dbc8396fa20
bamo100 <bankolediamond10@gmail.com>
2024-09-10 14:16:52 +0100

    fix: mddleware fix

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,7 @@
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/client"]);
+const isProtectedRoute = createRouteMatcher([]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
